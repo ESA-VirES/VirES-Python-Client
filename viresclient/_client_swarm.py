5eb9a7db16ef50fa75ecdb7e10f3b727cea7de70
Ashley Smith <ashley.smith@magneticearth.org>
2020-01-24 12:42:39 +0000

    Removed deprecated AMPS-variables as auxiliaries.

--- viresclient/_client_swarm.py
+++ viresclient/_client_swarm.py
@@ -406,8 +406,7 @@ class SwarmRequest(ClientRequest):
         "QDLon", "QDBasis", "MLT", "SunDeclination", "SunHourAngle",
         "SunRightAscension", "SunAzimuthAngle", "SunZenithAngle",
         "SunLongitude", "SunVector", "DipoleAxisVector", "NGPLatitude",
-        "NGPLongitude", "DipoleTiltAngle", "UpwardCurrent", "TotalCurrent",
-        "DivergenceFreeCurrentFunction",
+        "NGPLongitude", "DipoleTiltAngle",
         ]
 
     MAGNETIC_MODEL_VARIABLES = ["F", "B_NEC"]
