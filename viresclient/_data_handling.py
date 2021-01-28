e6c78d524f56559c68fec9f8568692b7e03f657f
Ashley Smith <ashley.smith@magneticearth.org>
2021-01-28 12:46:06 +0000

    Fix #57 - usage of cdflib.cdfread with v0.3.20

--- viresclient/_data_handling.py
+++ viresclient/_data_handling.py
@@ -111,9 +111,13 @@ class FileReader(object):
     @staticmethod
     def _open_cdf(file):
         try:
-            return cdflib.CDF(file.name)
+            f = file.name
         except AttributeError:
-            return cdflib.CDF(file)
+            f = file
+        try:
+            return cdflib.cdfread.CDF(f, string_encoding="utf-8")
+        except TypeError:
+            return cdflib.cdfread.CDF(f)
 
     @staticmethod
     def _ensure_list(attribute):
@@ -332,7 +336,7 @@ class ReturnedDataFile(object):
     def open_cdf(self):
         """Returns the opened file as cdflib.CDF
         """
-        return cdflib.CDF(self._file.name)
+        return FileReader.open_cdf(self._file.name)
 
     def _write_new_data(self, data):
         """Replace the tempfile contents with 'data' (bytes)
