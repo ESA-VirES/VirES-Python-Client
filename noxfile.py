51b0c85fa780a98add5a6f3073bbc5db60f180ef
Ashley Smith <ashley.smith@magneticearth.org>
2022-04-25 15:05:45 +0100

    Configure linting with pre-commit

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,15 @@
 import nox
 
+
+@nox.session
+def lint(session):
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     session.install(".")
