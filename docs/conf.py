3b5ba0a128a834ced020606dc248ce6f98f27e05
Ashley Smith <ashley.smith@magneticearth.org>
2022-05-13 11:51:01 +0100

    Add new EFI products (#82)
    * Support for SW_EXPT_EFIx_TCTyy collections.
    
    * Support for SW_OPER_EFIxTIE_2_ collections.
    
    * Rename EFI_TII_TCyy to EFI_TCTyy
    
    * Add references for EFI_TIE and EFI_TCTyy
    
    * Fix different variable description var "CATDESC"
    
    * Add forgotten Viy var
    
    * Add TIE & TCT to docs
    
    * Revert to sphinx-book-theme
    
    * Disable pre-commit flake8 "print" check
    
    * Add changelog for new products for 0.10.3
    
    * Metadata with CITATION.cff that Zenodo should read
    
    Co-authored-by: Martin Paces <martin.paces@eox.at>

--- docs/conf.py
+++ docs/conf.py
@@ -71,13 +71,13 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"style_external_links": True}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
