94249e5ca37782225514b35aa12ee877bd644b1d
Ashley Smith <ashley.smith@ed.ac.uk>
2025-12-04 13:57:13 +0000

    Begin changelog for v0.14

--- src/viresclient/__init__.py
+++ src/viresclient/__init__.py
@@ -35,4 +35,4 @@ from ._client_swarm import SwarmRequest
 from ._config import ClientConfig, set_token
 from ._data_handling import ReturnedData, ReturnedDataFile
 
-__version__ = "0.13.0"
+__version__ = "0.14.0"
